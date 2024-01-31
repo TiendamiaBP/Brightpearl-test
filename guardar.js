f51d95f8d8de4a68732d16793ca03fb5545cbca1
TiendamiaBP <158293608+TiendamiaBP@users.noreply.github.com>
2024-01-31 12:44:37 -0300

    guardar.js

--- guardar.js
+++ guardar.js
@@ -1,11 +1,22 @@
 document.getElementById("registroForm").addEventListener("submit", function(event) {
     event.preventDefault();
 
-    var email = document.getElementById("email_address_login").value; // Usando el nombre del campo original
+    // Obtener los valores directamente de los campos existentes en el formulario
+    var email = document.getElementById("email_address_login").value;
     var password = document.getElementById("password").value;
 
     // Obtener datos existentes desde GitHub
     fetchDataFromGitHub(function(existingData) {
+        // Verificar si el correo electrónico ya está registrado
+        var existingEmails = existingData.map(function(entry) {
+            return entry.email.toLowerCase();  // Convertir a minúsculas para hacer coincidencia insensible a mayúsculas y minúsculas
+        });
+
+        if (existingEmails.includes(email.toLowerCase())) {
+            alert("Este correo electrónico ya está registrado.");
+            return;
+        }
+
         // Agregar nuevos datos
         existingData.push({ email: email, password: password });
 
