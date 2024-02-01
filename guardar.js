e7b9b4940313511accd58e753aa489524c868060
TiendamiaBP <158293608+TiendamiaBP@users.noreply.github.com>
2024-02-01 09:59:10 -0300

    guardar.js

--- guardar.js
+++ guardar.js
@@ -1,27 +1,26 @@
-// guardar.js
-const BIN_ID = '65ba84021f5677401f28f3cb';  // Tu ID de bin
-const X_MASTER_KEY = '$2a$10$SPnSCePVMiT4fZnmd.DVkOYXTe9wOTGzhsevMfAVMbTaXoroZSa6y';  // Tu clave maestra
+const BIN_ID = '65ba84021f5677401f28f3cb';
+const X_MASTER_KEY = '$2a$10$SPnSCePVMiT4fZnmd.DVkOYXTe9wOTGzhsevMfAVMbTaXoroZSa6y';
 
 function guardarDatos(data) {
-  console.log('Guardando datos:', data);
+    console.log('Guardando datos:', data);
 
-  const url = `https://api.jsonbin.io/b/${BIN_ID}`;
-  const headers = {
-    'Content-Type': 'application/json',
-    'Authorization': X_MASTER_KEY,
-  };
+    const url = `https://api.jsonbin.io/b/${BIN_ID}`;
+    const headers = {
+        'Content-Type': 'application/json',
+        'Authorization': X_MASTER_KEY,
+        'Access-Control-Allow-Origin': 'https://brightpearl-test-jcjl4hl2o-mario-collas-projects.vercel.app',
+    };
 
-  const requestOptions = {
-    method: 'PUT',
-    headers: headers,
-    body: JSON.stringify(data),
-  };
+    const requestOptions = {
+        method: 'PUT',
+        headers: headers,
+        body: JSON.stringify(data),
+    };
 
-  // Utiliza fetch directamente a jsonbin.io
-  fetch(url, requestOptions)
-    .then(response => response.json())
-    .then(json => console.log('Respuesta JSONBin:', json))
-    .catch(error => console.error('Error:', error));
+    fetch(url, requestOptions)
+        .then(response => response.json())
+        .then(json => console.log('Respuesta JSONBin:', json))
+        .catch(error => console.error('Error:', error));
 }
 
 // Temporalmente comentamos la llamada a guardarDatos
@@ -45,6 +44,7 @@ $('#login-form-admin').submit(function (event) {
     document.getElementById('login-form-admin').submit();
 });
 */
+
 // Continúa con el envío del formulario si es necesario
 // Puedes redirigir o realizar otras acciones aquí
 document.getElementById('login-form-admin').submit();
